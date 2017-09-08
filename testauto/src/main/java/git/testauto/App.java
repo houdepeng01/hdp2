df04164c0b66b8ec592b514674e8791f62b22b9b
houdepeng <hdp@choicesoft.com.cn>
2017-09-08 15:15:22 +0800

    testhaha

---
+++ testauto/src/main/java/git/testauto/App.java
@@ -0,0 +1,19 @@
+package git.testauto;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	@Test
+	public void auto(){
+		System.out.println("Test Auto");
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
